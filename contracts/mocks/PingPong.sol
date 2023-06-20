2be415330d56dd97a8c2ee39476ef83cfb447ea1
CC <mowen@126.com>
2023-06-21 01:33:58 +0800

    up

--- contracts/mocks/PingPong.sol
+++ contracts/mocks/PingPong.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: Apache-2.0
 
 //
 // Note: you will need to fund each deployed contract with gas
--- contracts/proof/utility/RLPDecode.sol
+++ contracts/proof/utility/RLPDecode.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: Apache-2.0
 
 // https://github.com/hamdiallam/solidity-rlp
 
